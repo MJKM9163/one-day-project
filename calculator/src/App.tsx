f007ba7aa5957e816f97c350c745d57f272209ef
rhkrrbaud <rhkrrbaudgg@naver.com>
2022-07-07 18:07:33 +0900

    계산기 마이너스 문제 해결, 단축키 추가

--- calculator/src/App.tsx
+++ calculator/src/App.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { isConstructorDeclaration } from "typescript";
 import "./App.css";
 
 const CalculatorBox = styled.div`
@@ -111,37 +110,175 @@ const CalculatorBox = styled.div`
   }
 `;
 
-function App() {
-  const [resultValue, setResultValue] = useState("0");
-  console.log(resultValue);
-  const result = () => {
-    let nums: any = resultValue.match(/\d+/g); // 연결된 숫자 모두 찾기
-    let sign: any = resultValue.match(/\D/g); // 숫자가 아닌 것 모두 찾기
+let num: string = "0";
+let numArray: Array<number> = [];
+let signArray: Array<string> = [];
+let exe: boolean = false;
 
-    if (nums.length === sign.length) {
-      sign = sign.slice(0, sign.length - 1);
+function App() {
+  const [resultValue, setResultValue] = useState<string>("0");
+  const result = () => {
+    let final = 0;
+    if (numArray.length === signArray.length) {
+      console.log("같음!");
+      signArray.pop();
     }
-    console.log(nums);
-    let toNumber = nums.map(Number);
-    console.log(toNumber);
-    for (let i = 0; 1 !== toNumber.length; i++) {
-      console.log(sign);
-      if (sign[i] === "+") {
-        toNumber[0 + 1] = toNumber[0] + toNumber[0 + 1];
-      } else if (sign[i] === "-") {
-        toNumber[0 + 1] = toNumber[0] - toNumber[0 + 1];
-      } else if (sign[i] === "*") {
-        console.log(toNumber[0 + 1]);
-        console.log(sign);
-        toNumber[0 + 1] = toNumber[0] * toNumber[0 + 1];
-      } else if (sign[i] === "/") {
-        toNumber[0 + 1] = toNumber[0] / toNumber[0 + 1];
+
+    for (let i = 0; i < signArray.length; i++) {
+      if (signArray[i] === "+") {
+        final = numArray[i] + numArray[i + 1];
+      } else if (signArray[i] === "-") {
+        final = numArray[i] - numArray[i + 1];
+      } else if (signArray[i] === "*") {
+        final = numArray[i] * numArray[i + 1];
+      } else if (signArray[i] === "/") {
+        final = numArray[i] / numArray[i + 1];
       }
-      toNumber.shift();
     }
-    console.log(toNumber[0]);
-    setResultValue(String(toNumber[0]));
+    numArray = [];
+    signArray = [];
+    numArray = [final];
+    exe = true;
+    return setResultValue(String(final));
   };
+
+  const keyEvent = (e: KeyboardEvent) => {
+    if (e.key === "0") {
+      if (resultValue !== "0" && !isNaN(Number(resultValue[resultValue.length - 1]))) {
+        setResultValue(resultValue + "0");
+        num += "0";
+      }
+    } else if (e.key === "1") {
+      if (resultValue === "0" || exe) {
+        exe = false;
+        numArray = [];
+        setResultValue("1");
+      } else {
+        setResultValue(resultValue + "1");
+      }
+      num += "1";
+    } else if (e.key === "2") {
+      if (resultValue === "0" || exe) {
+        exe = false;
+        numArray = [];
+        setResultValue("2");
+      } else {
+        setResultValue(resultValue + "2");
+      }
+      num += "2";
+    } else if (e.key === "3") {
+      if (resultValue === "0" || exe) {
+        exe = false;
+        numArray = [];
+        setResultValue("3");
+      } else {
+        setResultValue(resultValue + "3");
+      }
+      num += "3";
+    } else if (e.key === "4") {
+      if (resultValue === "0" || exe) {
+        exe = false;
+        numArray = [];
+        setResultValue("4");
+      } else {
+        setResultValue(resultValue + "4");
+      }
+      num += "4";
+    } else if (e.key === "5") {
+      if (resultValue === "0" || exe) {
+        exe = false;
+        numArray = [];
+        setResultValue("5");
+      } else {
+        setResultValue(resultValue + "5");
+      }
+      num += "5";
+    } else if (e.key === "6") {
+      if (resultValue === "0" || exe) {
+        exe = false;
+        numArray = [];
+        setResultValue("6");
+      } else {
+        setResultValue(resultValue + "6");
+      }
+      num += "6";
+    } else if (e.key === "7") {
+      if (resultValue === "0" || exe) {
+        exe = false;
+        numArray = [];
+        setResultValue("7");
+      } else {
+        setResultValue(resultValue + "7");
+      }
+      num += "7";
+    } else if (e.key === "8") {
+      if (resultValue === "0" || exe) {
+        exe = false;
+        numArray = [];
+        setResultValue("8");
+      } else {
+        setResultValue(resultValue + "8");
+      }
+      num += "8";
+    } else if (e.key === "9") {
+      if (resultValue === "0" || exe) {
+        exe = false;
+        numArray = [];
+        setResultValue("9");
+      } else {
+        setResultValue(resultValue + "9");
+      }
+      num += "9";
+    } else if (e.key === "+") {
+      exe = false;
+      if (!isNaN(Number(resultValue[resultValue.length - 1]))) {
+        setResultValue(resultValue + "+");
+        numArray = num !== "" ? [...numArray, Number(num)] : [...numArray];
+        signArray = [...signArray, "+"];
+        num = "";
+      }
+    } else if (e.key === "-") {
+      exe = false;
+      if (num === "0") {
+        setResultValue(resultValue + "-");
+        numArray = [0];
+        signArray = [...signArray, "-"];
+        num = "";
+      } else if (!isNaN(Number(resultValue[resultValue.length - 1]))) {
+        setResultValue(resultValue + "-");
+        numArray = num !== "" ? [...numArray, Number(num)] : [...numArray];
+        signArray = [...signArray, "-"];
+        num = "";
+      }
+    } else if (e.key === "*") {
+      exe = false;
+      if (!isNaN(Number(resultValue[resultValue.length - 1]))) {
+        setResultValue(resultValue + "*");
+        numArray = num !== "" ? [...numArray, Number(num)] : [...numArray];
+        signArray = [...signArray, "*"];
+        num = "";
+      }
+    } else if (e.key === "/") {
+      exe = false;
+      if (!isNaN(Number(resultValue[resultValue.length - 1]))) {
+        setResultValue(resultValue + "/");
+        numArray = num !== "" ? [...numArray, Number(num)] : [...numArray];
+        signArray = [...signArray, "/"];
+        num = "";
+      }
+    } else if (e.key === "Enter") {
+      if (num !== "") {
+        numArray = [...numArray, Number(num)];
+      }
+      num = "";
+      result();
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("keypress", keyEvent);
+
+    return () => document.removeEventListener("keypress", keyEvent);
+  });
 
   return (
     <CalculatorBox>
@@ -158,6 +295,7 @@ function App() {
                   } else {
                     setResultValue(resultValue + "1");
                   }
+                  num += "1";
                 }}
               >
                 1
@@ -170,6 +308,7 @@ function App() {
                   } else {
                     setResultValue(resultValue + "2");
                   }
+                  num += "2";
                 }}
               >
                 2
@@ -182,6 +321,7 @@ function App() {
                   } else {
                     setResultValue(resultValue + "3");
                   }
+                  num += "3";
                 }}
               >
                 3
@@ -194,6 +334,7 @@ function App() {
                   } else {
                     setResultValue(resultValue + "4");
                   }
+                  num += "4";
                 }}
               >
                 4
@@ -206,6 +347,7 @@ function App() {
                   } else {
                     setResultValue(resultValue + "5");
                   }
+                  num += "5";
                 }}
               >
                 5
@@ -218,6 +360,7 @@ function App() {
                   } else {
                     setResultValue(resultValue + "6");
                   }
+                  num += "6";
                 }}
               >
                 6
@@ -230,6 +373,7 @@ function App() {
                   } else {
                     setResultValue(resultValue + "7");
                   }
+                  num += "7";
                 }}
               >
                 7
@@ -242,6 +386,7 @@ function App() {
                   } else {
                     setResultValue(resultValue + "8");
                   }
+                  num += "8";
                 }}
               >
                 8
@@ -254,6 +399,7 @@ function App() {
                   } else {
                     setResultValue(resultValue + "9");
                   }
+                  num += "9";
                 }}
               >
                 9
@@ -261,8 +407,9 @@ function App() {
               <div
                 className="num"
                 onClick={() => {
-                  if (resultValue !== "0") {
+                  if (resultValue !== "0" && !isNaN(Number(resultValue[resultValue.length - 1]))) {
                     setResultValue(resultValue + "0");
+                    num += "0";
                   }
                 }}
               >
@@ -271,6 +418,10 @@ function App() {
               <div
                 className="num"
                 onClick={() => {
+                  if (num !== "") {
+                    numArray = [...numArray, Number(num)];
+                  }
+                  num = "";
                   result();
                 }}
               >
@@ -282,6 +433,9 @@ function App() {
                 className="sign"
                 onClick={() => {
                   setResultValue("0");
+                  numArray = [];
+                  signArray = [];
+                  num = "0";
                 }}
               >
                 지우기
@@ -291,6 +445,9 @@ function App() {
                 onClick={() => {
                   if (!isNaN(Number(resultValue[resultValue.length - 1]))) {
                     setResultValue(resultValue + "+");
+                    numArray = num !== "" ? [...numArray, Number(num)] : [...numArray];
+                    signArray = [...signArray, "+"];
+                    num = "";
                   }
                 }}
               >
@@ -299,8 +456,16 @@ function App() {
               <div
                 className="sign"
                 onClick={() => {
-                  if (!isNaN(Number(resultValue[resultValue.length - 1]))) {
+                  if (num === "0") {
                     setResultValue(resultValue + "-");
+                    numArray = [0];
+                    signArray = [...signArray, "-"];
+                    num = "";
+                  } else if (!isNaN(Number(resultValue[resultValue.length - 1]))) {
+                    setResultValue(resultValue + "-");
+                    numArray = num !== "" ? [...numArray, Number(num)] : [...numArray];
+                    signArray = [...signArray, "-"];
+                    num = "";
                   }
                 }}
               >
@@ -311,6 +476,9 @@ function App() {
                 onClick={() => {
                   if (!isNaN(Number(resultValue[resultValue.length - 1]))) {
                     setResultValue(resultValue + "*");
+                    numArray = num !== "" ? [...numArray, Number(num)] : [...numArray];
+                    signArray = [...signArray, "*"];
+                    num = "";
                   }
                 }}
               >
@@ -321,6 +489,9 @@ function App() {
                 onClick={() => {
                   if (!isNaN(Number(resultValue[resultValue.length - 1]))) {
                     setResultValue(resultValue + "/");
+                    numArray = num !== "" ? [...numArray, Number(num)] : [...numArray];
+                    signArray = [...signArray, "/"];
+                    num = "";
                   }
                 }}
               >
