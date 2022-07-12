c1266cf85892f4f373bb90797f5f2d0587160065
kkm <rhkrrbaudgg@naver.com>
2022-07-12 21:57:29 +0900

    branch 코드 작성 중

--- texttoy/src/App.tsx
+++ texttoy/src/App.tsx
@@ -1,27 +1,49 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 
+interface branchType {
+  commit: Array<Array<string>>;
+  message: Array<string>;
+}
+
+interface commitType {
+  file: Array<Array<string>>;
+  commitMessage: Array<string>;
+}
+
 function App() {
   const [code, setCode] = useState("");
   const [staging, setStaging] = useState<Array<string>>([]);
-  const [branch, setBranch] = useState<Array<Array<string>>>([]);
-  const [commit, setCommit] = useState([]);
+  const [branch, setBranch] = useState<branchType>({ commit: [], message: [] });
+  const [commit, setCommit] = useState<commitType>({ file: [], commitMessage: [] });
   const ref = useRef<HTMLInputElement>(null);
 
   const codeCheck = (v: string) => {
     // code 재작성 하기
     let [, command, name] = v.split(" ");
+    let optionCheck = /\-\w/;
+
     if (command === "add") {
       setStaging([...staging, name]);
     } else if (command === "push") {
       if (staging.length > 0) {
-        setBranch([...branch, [...commit]]);
-        setCommit([]);
+        setBranch({
+          commit: [...branch.commit, ...commit.file],
+          message: [...branch.message, ...commit.commitMessage],
+        });
+        setCommit({ file: [], commitMessage: [] });
       } else {
         // commit이 없다는 메세지
       }
-    } else if (command === "commit") {
-      setCommit([]);
+    } else if (command === "commit" && /\-m/.test(v)) {
+      // 옵션이 있을 경우만 넘어감
+      let [gitCommand, messageText] = v.split(optionCheck);
+      let [git, command] = gitCommand.split(" ");
+      //console.log(git);
+      //console.log(command);
+      console.log(messageText);
+
+      setCommit({ file: [...commit.file, [...staging]], commitMessage: [...commit.commitMessage, messageText] });
       setStaging([]);
       // commit code 작성
     }
