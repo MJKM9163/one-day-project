f3656fd2064f65c2823005b8a9aaa9eb1a08f63d
MJKM <rhkrrbaudgg@naver.com>
2022-08-10 20:35:27 +0900

    테스트 연습

--- promise/src/App.tsx
+++ promise/src/App.tsx
@@ -24,29 +24,6 @@ function App() {
   };
   test().then((e) => console.log(e));
 
-  function numberSearch(str: any) {
-    // TODO: 여기에 코드를 작성합니다.
-    let num = 0;
-    let text = [];
-    for (let i = 0; i < str.length; i++) {
-      if (Number(str[i])) {
-        num += Number(str[i]);
-      } else if (!Number(str[i]) && str[i] !== " ") {
-        text.push(str[i]);
-      }
-    }
-    return Math.round(num / text.length);
-  }
-
-  let output = numberSearch("Hello6 ");
-  console.log(output); // --> 1
-
-  output = numberSearch("Hello6 9World 2,");
-  console.log(output); // --> 2
-
-  output = numberSearch("Hello6 9World 2, Nic8e D7ay!");
-  console.log(output); // --> 2
-
   return (
     <div>
       <div id="userInfo"></div>
