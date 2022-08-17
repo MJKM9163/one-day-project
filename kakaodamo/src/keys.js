a22f7a0e4cfd49ad42a8a386a27d8c055a6d28a4
MJKM <rhkrrbaudgg@naver.com>
2022-08-17 09:53:18 +0900

    민감한 정보 제거

--- kakaodamo/src/keys.js
+++ kakaodamo/src/keys.js
@@ -1,4 +1,1 @@
-export const CLIENT_ID = "5b8577561d76bdc98d6a4fcf3c087b17";
-export const REDIRECT_URI = "http://localhost:3000/auth/kakao/callback";
-export const CLIENT_SECRET = "1ZJfdwTviZfwMO3rnDyEyrRovtFYyy8e";
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=code`;
