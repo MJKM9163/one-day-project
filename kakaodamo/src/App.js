a86356492f4eb37346c3b88bea7241f4cb848c03
MJKM <rhkrrbaudgg@naver.com>
2022-08-17 09:34:18 +0900

    App.js 코드 수정

--- kakaodamo/src/App.js
+++ kakaodamo/src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
-import Handler from "./pages/Handler";
 import Auth from "./pages/Auth";
 import Profile from "./pages/Profile";
 
@@ -9,10 +8,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route exact path="/signin" component={Signin} /> */}
         <Route exact path="/" element={<Home />} />
         <Route path="/auth/kakao/callback" element={<Auth />} />
-        {/* <Route path="/auth/kakao/callback" element={<Handler />} /> */}
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
