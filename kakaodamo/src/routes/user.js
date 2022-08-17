7ea8bac625936756bdecf668322ab0cf734c7469
MJKM <rhkrrbaudgg@naver.com>
2022-08-17 09:52:25 +0900

    파일 정리

--- kakaodamo/src/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const passport = require("passport");
-const userController = require("../controller/user");
-
-router.get("/kakao", passport.authenticate("kakao"));
-router.get("/kakao/callback", userController.kakaoCallback);
-
-module.exports = router;
