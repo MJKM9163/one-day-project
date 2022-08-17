1355164360424e28755b10aadcb7ad77edb80ec7
MJKM <rhkrrbaudgg@naver.com>
2022-08-17 09:53:02 +0900

    환경변수 연결

--- kakaodamo/src/pages/Auth.js
+++ kakaodamo/src/pages/Auth.js
@@ -1,9 +1,8 @@
-import React from "react";
 import { useEffect } from "react";
 import axios from "axios";
 import qs from "qs";
 import { useNavigate } from "react-router-dom";
-import { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } from "../keys";
+//import { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } from "../keys";
 const Auth = () => {
   const code = new URL(window.location.href).searchParams.get("code");
 
@@ -11,10 +10,10 @@ const Auth = () => {
   const getToken = async () => {
     const payload = qs.stringify({
       grant_type: "authorization_code",
-      client_id: CLIENT_ID,
-      redirect_uri: REDIRECT_URI,
+      client_id: process.env.REACT_APP_CLIENT_ID,
+      redirect_uri: process.env.REACT_APP_REDIRECT_URI,
       code: code,
-      client_secret: CLIENT_SECRET,
+      client_secret: process.env.REACT_APP_CLIENT_SECRET,
     });
     try {
       // access token 가져오기
@@ -24,7 +23,7 @@ const Auth = () => {
       );
 
       // Kakao Javascript SDK 초기화
-      window.Kakao.init(CLIENT_ID);
+      window.Kakao.init(process.env.REACT_APP_CLIENT_ID);
       // access token 설정
       window.Kakao.Auth.setAccessToken(res.data.access_token);
       navigate("/profile");
